7c5aeed01b6940e32dcfa024e3a5ab73c31febd0
ArshJ <arshrjoshi@gmail.com>
2020-03-26 19:25:37 -0700

    made corrections

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,7 @@
+import os
+
+
 DEBUG = False
 SECRET_KEY = '381blackwell'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:381blackwell@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['postgresql://postgres:381blackwell@localhost/catalog_db']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
